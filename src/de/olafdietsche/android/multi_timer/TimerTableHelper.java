de8038c832cd58f2a100e7ff48ab08405abb354d
Olaf Dietsche <olafdietsche@users.noreply.github.com>
2016-02-10 19:21:37 +0100

    Refactor timer table data object

--- src/de/olafdietsche/android/multi_timer/TimerTableHelper.java
+++ src/de/olafdietsche/android/multi_timer/TimerTableHelper.java
@@ -3,6 +3,7 @@
 
 package de.olafdietsche.android.multi_timer;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 import de.olafdietsche.android.database.TableHelper;
@@ -14,6 +15,84 @@ public class TimerTableHelper extends TableHelper {
 	public static final String COLUMN_NAME_DURATION = "duration";
 	public static final String COLUMN_NAME_REMAINING = "remaining";
 	public static final String COLUMN_NAME_DEADLINE = "deadline";
+
+	public static class Data {
+		public Data(Cursor cursor) {
+			id = cursor.getLong(idIndex);
+			name = cursor.getString(nameIndex);
+			duration = cursor.getLong(durationIndex);
+
+			pausing = !cursor.isNull(remainingIndex);
+			if (pausing)
+				remaining = cursor.getLong(remainingIndex);
+
+			running = !cursor.isNull(deadlineIndex);
+			if (running)
+				deadline = cursor.getLong(deadlineIndex);
+		}
+
+		public void startTimer() {
+			if (!isRunning()) {
+				long now = System.currentTimeMillis() / 1000;
+				deadline = now + duration;
+				running = true;
+				pausing = false;
+			}
+		}
+
+		public void stopTimer() {
+			running = pausing = false;
+		}
+
+		public void pauseTimer() {
+			if (!isRunning())
+				return;
+
+			long now = System.currentTimeMillis() / 1000;
+			remaining = deadline - now;
+			running = false;
+			pausing = true;
+		}
+
+		public void resumeTimer() {
+			if (!isPausing())
+				return;
+
+			long now = System.currentTimeMillis() / 1000;
+			deadline = now + remaining;
+			pausing = false;
+			running = true;
+		}
+
+		public boolean isRunning() {
+			return running;
+		}
+
+		public boolean isPausing() {
+			return pausing;
+		}
+
+		public String toString() {
+			return "{TimerTableHelper.Data: " + name + ", " + duration + ", " + pausing + "/" + remaining + ", " + running + "/" + deadline + "}";
+		}
+
+		final long id;
+		String name;
+		long duration;
+		long remaining;
+		long deadline;
+		boolean running, pausing;
+
+		private static final int idIndex = 0, nameIndex = 1, durationIndex = 2, remainingIndex = 3, deadlineIndex = 4;
+
+		public static final String[] projection = new String[] {
+			BaseColumns._ID,
+			TimerTableHelper.COLUMN_NAME_NAME,
+			TimerTableHelper.COLUMN_NAME_DURATION,
+			TimerTableHelper.COLUMN_NAME_REMAINING,
+			TimerTableHelper.COLUMN_NAME_DEADLINE,
+		};
+	}
 
 	public TimerTableHelper(SQLiteOpenHelper dbHelper) {
 		super(dbHelper, TABLE_NAME);
