6c4701cc7607c12c4ed7f2264551ec53c6a6afd4
Olaf Dietsche <olafdietsche@users.noreply.github.com>
2016-02-24 19:36:28 +0100

    Add notifications

--- src/de/olafdietsche/android/multi_timer/AlarmReceiver.java
+++ src/de/olafdietsche/android/multi_timer/AlarmReceiver.java
@@ -4,6 +4,8 @@
 package de.olafdietsche.android.multi_timer;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -14,6 +16,12 @@ import android.os.Bundle;
 public class AlarmReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		long id = intent.getLongExtra("id", -1);
+		String title = intent.getStringExtra("name");
+		long when = intent.getLongExtra("deadline", -1);
+		Notification notification = makeNotification(context, title, when);
+		NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		nm.notify((int) id, notification);
 	}
 
 	public static void scheduleAlarm(final Context context, final TimerTableHelper.Data data) {
@@ -26,6 +34,8 @@ public class AlarmReceiver extends BroadcastReceiver {
 		PendingIntent pendingIntent = makePendingIntent(context, data);
 		AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 		am.cancel(pendingIntent);
+		NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		nm.cancel((int) data.id);
 	}
 
 	private static PendingIntent makePendingIntent(final Context context, final TimerTableHelper.Data data) {
@@ -35,5 +45,17 @@ public class AlarmReceiver extends BroadcastReceiver {
 		Bundle bundle = data.toBundle();
 		intent.putExtras(bundle);
 		return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+	}
+
+	private static Notification makeNotification(final Context context, final String message, final long when) {
+		Notification notification = new Notification(android.R.drawable.ic_lock_idle_alarm, message, when * 1000);
+		Intent contentIntent = new Intent(context, MainActivity.class);
+		contentIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+		PendingIntent pendingIntent =
+			PendingIntent.getActivity(context, 0, contentIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+		String title = context.getResources().getString(R.string.app_name);
+		notification.setLatestEventInfo(context, title, message, pendingIntent);
+		return notification;
 	}
 }
