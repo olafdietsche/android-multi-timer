51f86fe9bedee828dc0fdcfa0836a33c2d6edd89
Olaf Dietsche <olafdietsche@users.noreply.github.com>
2016-02-29 16:26:43 +0100

    Silence alarm sound after short time

--- src/de/olafdietsche/android/multi_timer/AlarmReceiver.java
+++ src/de/olafdietsche/android/multi_timer/AlarmReceiver.java
@@ -22,8 +22,13 @@ public class AlarmReceiver extends BroadcastReceiver {
 		long id = intent.getLongExtra(BaseColumns._ID, -1);
 		String message = intent.getStringExtra(TimerTableHelper.COLUMN_NAME_NAME);
 		long when = intent.getLongExtra(TimerTableHelper.COLUMN_NAME_DEADLINE, -1);
-		acquireWakeLock(context);
-		showNotification(context, id, message, when);
+		boolean silence = intent.getBooleanExtra(EXTRA_SILENCE_NOTIFICATION, false);
+		if (silence) {
+			silenceAlarm(context, id, message, when);
+		} else {
+			acquireWakeLock(context);
+			showNotification(context, id, message, when);
+		}
 	}
 
 	public static void scheduleAlarm(final Context context, final TimerTableHelper.Data data) {
@@ -50,8 +55,36 @@ public class AlarmReceiver extends BroadcastReceiver {
 
 	private static void showNotification(final Context context, long id, String message, long when) {
 			Notification notification = makeNotification(context, message, when);
+			notification.sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+			notification.vibrate = new long[]{ 100, 750, 50, 750, 50, 750 };
 			NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 			nm.notify((int) id, notification);
+			scheduleSilence(context, id, message, when);
+	}
+
+	private static void scheduleSilence(final Context context, long id, String message, long when) {
+		PendingIntent pendingIntent = makeSilenceIntent(context, id, message, when);
+		long now = System.currentTimeMillis();
+		AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		am.set(AlarmManager.RTC_WAKEUP, now + SOUND_TIMEOUT, pendingIntent);
+	}
+
+	private static void silenceAlarm(final Context context, long id, String message, long when) {
+		Notification notification = makeNotification(context, message, when);
+		NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		nm.cancel((int) id);
+		nm.notify((int) id, notification);
+	}
+
+	private static PendingIntent makeSilenceIntent(final Context context, long id, String message, long when) {
+		Intent intent = new Intent(context, AlarmReceiver.class);
+		Uri uri = Uri.fromParts("android-app", MainActivity.PACKAGE_NAME + "/" + id, null);
+		intent.setData(uri);
+		intent.putExtra(BaseColumns._ID, id);
+		intent.putExtra(TimerTableHelper.COLUMN_NAME_NAME, message);
+		intent.putExtra(TimerTableHelper.COLUMN_NAME_DEADLINE, when);
+		intent.putExtra(EXTRA_SILENCE_NOTIFICATION, true);
+		return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 	}
 
 	private static PendingIntent makeAlarmIntent(final Context context, final TimerTableHelper.Data data) {
@@ -74,8 +107,6 @@ public class AlarmReceiver extends BroadcastReceiver {
 
 	private static Notification makeNotification(final Context context, final String message, final long when) {
 		Notification notification = new Notification(android.R.drawable.ic_lock_idle_alarm, message, when * 1000);
-		notification.sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-		notification.vibrate = new long[]{ 100, 750, 50, 750, 50, 750 };
 		String title = context.getResources().getString(R.string.app_name);
 		PendingIntent pendingIntent = makeNotificationIntent(context);
 		notification.setLatestEventInfo(context, title, message, pendingIntent);
@@ -97,5 +128,7 @@ public class AlarmReceiver extends BroadcastReceiver {
 
 	private static final String TAG = AlarmReceiver.class.getName();
 	private static final String EXTRA_CLEAR_NOTIFICATION = MainActivity.PACKAGE_NAME + ".intent.extra.CLEAR_NOTIFICATION";
+	private static final String EXTRA_SILENCE_NOTIFICATION = MainActivity.PACKAGE_NAME + ".intent.extra.SILENCE_NOTIFICATION";
 	private static final long WAKELOCK_TIMEOUT = 15000;
+	private static final long SOUND_TIMEOUT = 15000;
 }
